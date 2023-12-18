604d84ac982f0be71452f5ecf363cd2da6eb9e97
MatiasSimonLuduena <matipax04@gmail.com>
2023-12-17 23:41:29 -0300

    v0.0.2

--- src/data.js
+++ src/data.js
@@ -1,20 +1,34 @@
 export const products = [
     {
-        img: "fb-1.jpeg", title: "Short Dino", stars: 2, sex: "Nene", talle: "5", id: 1
+        img: "products/fb-1.jpeg", title: "Short Dino", stars: 1, sex: "Nene", talle: "4/5", id: 1,
+        categories: [ "Shorts", "Verano" ]
     },
     {
-        img: "fb-2.jpeg", title: "Short Futbol", stars: 2, sex: "Nene", talle: "5", id: 2
+        img: "products/fb-2.jpeg", title: "Short Futbol", stars: 1, sex: "Nene", talle: "4/5", id: 2,
+        categories: [ "Shorts", "Verano" ]
     },
     {
-        img: "zyro-1.jpg", title: "Remera Volar", stars: 1, sex: "Nene", talle: "5/6", id: 3
+        img: "products/fb-3.jpg", title: "Short Cuadro", stars: 1, sex: "Nene", talle: "5/6", id: 3,
+        categories: [ "Shorts", "Verano" ]
     },
     {
-        img: "zyro-2.jpg", title: "Short Rallado", stars: 2, sex: "Nene", talle: "4/5", id: 4
+        img: "products/fb-4.jpg", title: "Campera Jean", stars: 1, sex: "Nene/a", talle: "4/5", id: 4,
+        categories: [ "Camperas", "Invierno" ]
     },
     {
-        img: "zyro-3.jpg", title: "Remera bolsillo", stars: 1, sex: "Nene", talle: "5/6", id: 5
+        img: "products/fb-5.jpg", title: "Remera L Dino", stars: 1, sex: "Nene", talle: "4/5", id: 5,
+        categories: [ "Remeras Largas", "Invierno" ]
     },
     {
-        img: "zyro-4.jpg", title: "Camiseta Argentina", stars: 2, sex: "Nene", talle: "5/6", id: 6
-    }
+        img: "products/fb-6.jpg", title: "Body L Zorro", stars: 1, sex: "Nene", talle: "3", id: 6,
+        categories: [ "Bodies", "Invierno" ]
+    },
+    {
+        img: "products/zyro-1.jpg", title: "Remera Volar", stars: 1, sex: "Nene", talle: "6", id: 7,
+        categories: [ "Remeras", "Verano" ]
+    },
+    {
+        img: "products/zyro-2.jpg", title: "Short Rallas", stars: 1, sex: "Nene", talle: "5/6", id: 8,
+        categories: [ "Shorts", "Verano" ]
+    },
 ]
